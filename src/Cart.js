74baaadf3ab49cee20936e3adc4255f651013e4f
cm <carloshrmoraes@gmail.com>
2022-02-06 20:10:41 -0300

    cart total and small adjustments

--- src/Cart.js
+++ src/Cart.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./styles/Cart.css";
 
 function Cart({ cartItems, cartSetter }) {
+    const [totalPrice, setTotalPrice] = useState(0);
     function removeFromCart(id) {
         let filteredCart = cartItems.filter((po) => po.id !== id);
         cartSetter(filteredCart);
@@ -13,14 +14,24 @@ function Cart({ cartItems, cartSetter }) {
         cartSetter([...newCart]);
     }
 
+    useEffect(() => {
+        let calculatedTotal = 0;
+        cartItems.forEach((poke) => {
+            calculatedTotal += poke.info.base_experience * poke.quantity;
+        });
+        setTotalPrice(calculatedTotal);
+    });
+
     return (
         <div id="shopping_cart_div">
             <h3>Your Cart: </h3>
             <ul>
                 {cartItems.map((item, i) => (
-                    <div key={item.id}>
-                        <li>{item.info.name}</li>
+                    <li key={item.id}>
+                        <p>{item.info.name}</p>
+                        <label htmlFor="amount">Amount: </label>
                         <input
+                            name="amount"
                             type="number"
                             min="1"
                             max="99"
@@ -28,10 +39,11 @@ function Cart({ cartItems, cartSetter }) {
                             onChange={(e) => changeQuantity(e, i)}
                         />
                         <button onClick={() => removeFromCart(item.id)}>Remove</button>
-                        <p>Price: {item.info.base_experience * item.quantity}</p>
-                    </div>
+                        <p>Subtotal: {item.info.base_experience * item.quantity}</p>
+                    </li>
                 ))}
             </ul>
+            <p>Total Price: {totalPrice}</p>
         </div>
     );
 }
