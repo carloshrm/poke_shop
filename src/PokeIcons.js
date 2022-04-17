5faf6ccd9c873b02db0dbfcd02dbea46476b7225
Carlos Moraes <carloshrmoraes@gmail.com>
2022-04-17 19:59:03 -0300

    implemented type icons in details pop-up

--- src/PokeIcons.js
+++ src/PokeIcons.js
@@ -1,39 +1,41 @@
-import { ReactComponent as Bug } from "./icons/Pokémon_Bug_Type_Icon.svg";
-import { ReactComponent as Dark } from "./icons/Pokémon_Dark_Type_Icon.svg";
-import { ReactComponent as Dragon } from "./icons/Pokémon_Dragon_Type_Icon.svg";
-import { ReactComponent as Electric } from "./icons/Pokémon_Electric_Type_Icon.svg";
-import { ReactComponent as Fairy } from "./icons/Pokémon_Fairy_Type_Icon.svg";
-import { ReactComponent as Fighting } from "./icons/Pokémon_Fighting_Type_Icon.svg";
-import { ReactComponent as Fire } from "./icons/Pokémon_Fire_Type_Icon.svg";
-import { ReactComponent as Flying } from "./icons/Pokémon_Flying_Type_Icon.svg";
-import { ReactComponent as Ghost } from "./icons/Pokémon_Ghost_Type_Icon.svg";
-import { ReactComponent as Grass } from "./icons/Pokémon_Grass_Type_Icon.svg";
-import { ReactComponent as Ground } from "./icons/Pokémon_Ground_Type_Icon.svg";
-import { ReactComponent as Ice } from "./icons/Pokémon_Ice_Type_Icon.svg";
-import { ReactComponent as Normal } from "./icons/Pokémon_Normal_Type_Icon.svg";
-import { ReactComponent as Posion } from "./icons/Pokémon_Poison_Type_Icon.svg";
-import { ReactComponent as Psychic } from "./icons/Pokémon_Psychic_Type_Icon.svg";
-import { ReactComponent as Rock } from "./icons/Pokémon_Rock_Type_Icon.svg";
-import { ReactComponent as Steel } from "./icons/Pokémon_Steel_Type_Icon.svg";
-import { ReactComponent as Water } from "./icons/Pokémon_Water_Type_Icon.svg";
+import bug from "./icons/Pokémon_Bug_Type_Icon.svg";
+import dark from "./icons/Pokémon_Dark_Type_Icon.svg";
+import dragon from "./icons/Pokémon_Dragon_Type_Icon.svg";
+import electric from "./icons/Pokémon_Electric_Type_Icon.svg";
+import fairy from "./icons/Pokémon_Fairy_Type_Icon.svg";
+import fighting from "./icons/Pokémon_Fighting_Type_Icon.svg";
+import fire from "./icons/Pokémon_Fire_Type_Icon.svg";
+import flying from "./icons/Pokémon_Flying_Type_Icon.svg";
+import ghost from "./icons/Pokémon_Ghost_Type_Icon.svg";
+import grass from "./icons/Pokémon_Grass_Type_Icon.svg";
+import ground from "./icons/Pokémon_Ground_Type_Icon.svg";
+import ice from "./icons/Pokémon_Ice_Type_Icon.svg";
+import normal from "./icons/Pokémon_Normal_Type_Icon.svg";
+import poison from "./icons/Pokémon_Poison_Type_Icon.svg";
+import psychic from "./icons/Pokémon_Psychic_Type_Icon.svg";
+import rock from "./icons/Pokémon_Rock_Type_Icon.svg";
+import steel from "./icons/Pokémon_Steel_Type_Icon.svg";
+import water from "./icons/Pokémon_Water_Type_Icon.svg";
 
-export {
-    Bug,
-    Dark,
-    Dragon,
-    Electric,
-    Fairy,
-    Fighting,
-    Fire,
-    Flying,
-    Ghost,
-    Grass,
-    Ground,
-    Ice,
-    Normal,
-    Posion,
-    Psychic,
-    Rock,
-    Steel,
-    Water,
+const icons = {
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water,
 };
+
+export default icons;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import CartContext from "./CartContext";
-import TypeIcons from "./PokeIcons";
+import icons from "./PokeIcons";
 
 import "./styles/Pokemon.css";
 
@@ -9,24 +9,24 @@ function Pokemon({ info, toggle }) {
     function toggleDisplay() {
         toggle((prev) => ({ ...prev, display: !prev.display }));
     }
+
     return (
         <div id="pokemon_details_container">
-            <h1>{info.name[0].toUpperCase() + info.name.substring(1) + "."}</h1>
+            <h1>
+                {info.name[0].toUpperCase() + info.name.substring(1)}
+                {info.types.map((t) => (
+                    <img className="type_icon" src={icons[t.type.name]} alt="" />
+                ))}
+            </h1>
             <img src={info.sprites.other["official-artwork"].front_default} alt="" />
             <div>
-                <p>Number: {info.id}</p>
-                <p>
+                <p id="pokemon_main_stats">
                     Weight:{" "}
-                    {info.weight * 100 > 1000 ? info.weight / 10 + "Kg" : info.weight * 100 + "g"}.
+                    {info.weight * 100 > 1000 ? info.weight / 10 + "Kg" : info.weight * 100 + "g"}
+                    Number: {info.id}
+                    Height: {info.height * 10}cm
                 </p>
-                <p>Height: {info.height * 10}cm.</p>
-                <table id="pokemon_stats_table">
-                    <thead>
-                        <tr>
-                            <td>Stat</td>
-                            <td>Value</td>
-                        </tr>
-                    </thead>
+                <table id="pokemon_detailed_stats">
                     <tbody>
                         {info.stats.map((unit, i) => (
                             <tr key={"row" + i}>
@@ -37,6 +37,7 @@ function Pokemon({ info, toggle }) {
                     </tbody>
                 </table>
             </div>
+            <div id="pokemon_buttons"></div>
             <button onClick={() => addToCart(info)}>Add to cart</button>
             <button onClick={toggleDisplay}>Back</button>
         </div>
